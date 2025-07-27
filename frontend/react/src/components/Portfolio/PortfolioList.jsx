01f498e473f1273916780367c2ef7dd91e5089cb
Fusan02 <saitoufutoshi0909@gmail.com>
2025-07-27 13:43:34 +0900

    修正

--- frontend/react/src/components/Portfolio/PortfolioList.jsx
+++ frontend/react/src/components/Portfolio/PortfolioList.jsx
@@ -172,7 +172,7 @@ const PortfolioList = () => {
               <line x1="12" y1="5" x2="12" y2="19"></line>
               <line x1="5" y1="12" x2="19" y2="12"></line>
             </svg>
-            新規作成
+            投稿する
           </button>
         </header>
 
